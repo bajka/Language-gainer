bb42a79f5934f5de5d7440a055ae0af76ae9d242
Mariusz Bajkowski <bajka.mariusz@gmail.com>
2018-12-22 18:29:42 +0100

    Setup navigator for application.

--- App.js
+++ App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import MainMenu from './components/main-menu'
+import AppContainer from './navigation/app-navigator';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <MainMenu></MainMenu>
-      </View>
+        <AppContainer/>
     );
   }
 }
