d33574807429f1524451d9089ffdf23c8d907d33
Mariusz Bajkowski <bajka.mariusz@gmail.com>
2019-01-09 19:41:57 +0100

    Disable button when needed.

--- components/main-menu/main-menu.js
+++ components/main-menu/main-menu.js
@@ -26,7 +26,7 @@ export default class MainMenu extends React.Component {
             <ViewHeader text="Language gainer"></ViewHeader>
             <ViewDescription text="Welcome Mariusz, what do you want to do today?"></ViewDescription>
             <MenuButton headerText='Practise words' description='Practise most  important words in chosen language' image={Images.languages} click={() => this.props.navigation.navigate('LearnWord')}></MenuButton>
-            <MenuButton headerText='Deep dive into language' description='Listen podcast and read texts in chosen language' image={Images.diving} click={this.openPodcastsList} disabled={true}></MenuButton>
+            <MenuButton headerText='Deep dive into language' description='Listen podcast and read texts in chosen language' image={Images.diving} disabled={true}></MenuButton>
             <MenuButton headerText='Create own course' description='Share your knowledge with other people' image={Images.creating} click={this.createCourse}></MenuButton>
             <View style={styles.signOutButton}>
                 <Button color="#47525E" title="Sign out" onPress={this.signOut}></Button>
